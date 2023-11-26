131476634b086b82bc459097c2adc2ef3e3ad2c0
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-26 13:16:36 +0300

    splash screen ekledim

---
+++ WFA_YemekKaloriHesap_Proje/SplashScreen.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace YemekKalori.UI
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 189);
+            button1.Name = "button1";
+            button1.Size = new Size(800, 29);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(336, 213);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(117, 91);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 297);
+            button2.Name = "button2";
+            button2.Size = new Size(800, 24);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Palatino Linotype", 22.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Info;
+            label1.Location = new Point(292, 73);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 49);
+            label1.TabIndex = 3;
+            label1.Text = "Eucalyptus";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Elephant", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Info;
+            label2.Location = new Point(0, 407);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 24);
+            label2.TabIndex = 4;
+            label2.Text = "Loading...";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(0, 491);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 26);
+            panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.CornflowerBlue;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(99, 34);
+            panel2.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 17;
+            // 
+            // SplashScreen
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MediumAquamarine;
+            ClientSize = new Size(800, 516);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SplashScreen";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SplashScreen";
+            Load += SplashScreen_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ WFA_YemekKaloriHesap_Proje/SplashScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YemekKalori.UI
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 1;
+
+            if (panel2.Width >= 516)
+            {
+                timer1.Stop();
+                
+            }
+        }
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
