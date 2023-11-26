8061c7f7d924a3e4a39f65d07a72bdabc4e5fc93
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-26 19:34:38 +0300

    meal class ı eklendi

---
+++ YemekKalori.Domain/Entities/Meal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekKalori.Domain.Entities
+{
+    public class Meal:BaseClass
+    {
+        public string Name { get; set; }
+
+        public int Calorie { get; set; }
+
+        public ICollection<Food> Foods { get; set; }
+
+    }
+}
