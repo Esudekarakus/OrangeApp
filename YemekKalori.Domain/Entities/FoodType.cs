34bac19e4a1c59f4fda458c9c8a8277401c5a5be
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-26 19:37:33 +0300

    foodtype eklendi

---
+++ YemekKalori.Domain/Entities/FoodType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekKalori.Domain.Entities
+{
+    public class FoodType:BaseClass
+    {
+        public string Name { get; set; }
+        public ICollection<Food> Foods { get; set; }
+    }
+}
