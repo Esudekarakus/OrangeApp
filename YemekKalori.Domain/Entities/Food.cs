7e714b49827df2808b8554029cf7965aa395d0be
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-26 19:32:05 +0300

    bghghhghg food class oluşturuldu

---
+++ YemekKalori.Domain/Entities/Food.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekKalori.Domain.Entities
+{
+    public class Food:BaseClass
+    {
+        public string Name { get; set; }
+
+        public string PortionType { get; set; }
+
+        public int Calorie { get; set; }
+
+        public decimal Portion { get; set; }
+
+        public Meal Meal { get; set; }
+
+        public int FatRate { get; set; }
+
+        public int CarbRate { get; set; }
+
+        public int ProteinRate { get; set; }
+        public int MealRefID { get; set; }
+
+        public ICollection<FoodType> FoodTypes { get; set; }
+    }
+}
