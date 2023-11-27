75a7709d9746099b9e62e2926dcda00410dfabb4
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-27 10:33:27 +0300

    starting screen eklendi

---
+++ WFA_YemekKaloriHesap_Proje/StartingScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFA_YemekKaloriHesap_Proje;
+
+namespace YemekKalori.UI
+{
+    public partial class StartingScreen : Form
+    {
+        public StartingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartingScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lLabelEveet_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Login login = new Login();
+            this.Close();
+            login.Show();
+        }
+
+        private void lLabelHayir_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            this.Close();
+            signUp.Show();
+        }
+    }
+}
