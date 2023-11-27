a3ebfb4a7af453a78296d879ad50648e2603b955
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-11-27 10:03:18 +0300

    splash screen tasarımı yaptım

--- WFA_YemekKaloriHesap_Proje/SplashScreen.Designer.cs
+++ WFA_YemekKaloriHesap_Proje/SplashScreen.Designer.cs
@@ -28,7 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label2 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Palatino Linotype", 22.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Info;
+            label1.Location = new Point(295, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 49);
+            label1.TabIndex = 0;
+            label1.Text = "Eucalyptus";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(-1, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(802, 29);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(-1, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(802, 29);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(370, 202);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(75, 52);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(-1, 426);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(802, 27);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.RoyalBlue;
+            panel2.Location = new Point(-1, 426);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(69, 24);
+            panel2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Palatino Linotype", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Info;
+            label2.Location = new Point(-1, 379);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 24);
+            label2.TabIndex = 6;
+            label2.Text = "Loading...";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 17;
+            timer1.Tick += timer1_Tick;
             // 
             // SplashScreen
             // 
@@ -36,12 +116,31 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MediumAquamarine;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "SplashScreen";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "SplashScreen";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
