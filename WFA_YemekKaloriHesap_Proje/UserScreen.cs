938abec20d0e09fa1b7451a585999a01461a0c49
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2023-11-28 17:10:48 +0300

    listbox item çekme

--- WFA_YemekKaloriHesap_Proje/UserScreen.cs
+++ WFA_YemekKaloriHesap_Proje/UserScreen.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YemekKalori.BLL.Services;
 using YemekKalori.Domain.Entities;
 
 namespace YemekKalori.UI
@@ -16,21 +17,54 @@ namespace YemekKalori.UI
         public UserScreen()
         {
             InitializeComponent();
+            userService = new UserService();
+            foodService = new FoodService();
         }
 
         public UserScreen(User user)
         {
             InitializeComponent();
             this.user = user;
+            userService = new UserService();
+            foodService = new FoodService();
         }
 
         User user;
-
+        UserService userService;
+        FoodService foodService;
         
 
         private void UserScreen_Load(object sender, EventArgs e)
         {
-
+            if (user.Diet == Domain.Enums.DietType.Classic) 
+            {
+                lstYemekler.DisplayMember = null;
+                lstYemekler.DataSource = foodService.GetClassicFood();
+                
+                //lstYemekler.ValueMember = "Id";
+            }
+            else if (user.Diet == Domain.Enums.DietType.Vegan) 
+            {
+                lstYemekler.DisplayMember = null;
+                lstYemekler.DataSource = foodService.GetVeganFoods();
+                
+                //lstYemekler.ValueMember= "Id";
+            }
+            else if (user.Diet == Domain.Enums.DietType.Vegetarian) 
+            {
+                lstYemekler.DisplayMember = null;
+                lstYemekler.DataSource = foodService.GetVegetarianFood();
+                
+                //lstYemekler.ValueMember = "Id";
+            }
+            else if (user.Diet == Domain.Enums.DietType.Carnivor) 
+            {
+                lstYemekler.DisplayMember = null;
+                lstYemekler.DataSource = foodService.GetCarnivoreFoods();
+                
+                //lstYemekler.ValueMember = "Id";
+            }
+            lstYemekler.Refresh();
         }
 
       
