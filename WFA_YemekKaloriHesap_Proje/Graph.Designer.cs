4797254128649ca9b09714f2d0663f9a0b250166
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-12-01 15:34:07 +0300

    form olusturuldu

--- WFA_YemekKaloriHesap_Proje/Graph.Designer.cs
+++ WFA_YemekKaloriHesap_Proje/Graph.Designer.cs
@@ -31,41 +31,89 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            cGunluk = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            cHaftalik = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            cMonthly = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)cGunluk).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cHaftalik).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cMonthly).BeginInit();
             SuspendLayout();
             // 
-            // chart1
+            // cGunluk
             // 
             chartArea1.Name = "ChartArea1";
-            chart1.ChartAreas.Add(chartArea1);
+            cGunluk.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            chart1.Legends.Add(legend1);
-            chart1.Location = new Point(49, 63);
-            chart1.Name = "chart1";
+            cGunluk.Legends.Add(legend1);
+            cGunluk.Location = new Point(33, 43);
+            cGunluk.Name = "cGunluk";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            chart1.Series.Add(series1);
-            chart1.Size = new Size(716, 375);
-            chart1.TabIndex = 0;
-            chart1.Text = "chart1";
+            series1.Name = "Daily";
+            cGunluk.Series.Add(series1);
+            cGunluk.Size = new Size(492, 279);
+            cGunluk.TabIndex = 0;
+            cGunluk.Text = "chart1";
+            // 
+            // cHaftalik
+            // 
+            chartArea2.Name = "ChartArea1";
+            cHaftalik.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            cHaftalik.Legends.Add(legend2);
+            cHaftalik.Location = new Point(723, 43);
+            cHaftalik.Name = "cHaftalik";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Weekly";
+            cHaftalik.Series.Add(series2);
+            cHaftalik.Size = new Size(552, 279);
+            cHaftalik.TabIndex = 1;
+            cHaftalik.Text = "chart1";
+            // 
+            // cMonthly
+            // 
+            chartArea3.Name = "ChartArea1";
+            cMonthly.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            cMonthly.Legends.Add(legend3);
+            cMonthly.Location = new Point(364, 357);
+            cMonthly.Name = "cMonthly";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Monthly";
+            cMonthly.Series.Add(series3);
+            cMonthly.Size = new Size(743, 375);
+            cMonthly.TabIndex = 2;
+            cMonthly.Text = "chart1";
             // 
             // Graph
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(chart1);
+            ClientSize = new Size(1436, 769);
+            Controls.Add(cMonthly);
+            Controls.Add(cHaftalik);
+            Controls.Add(cGunluk);
             Name = "Graph";
             Text = "Graph";
             Load += Graph_Load;
-            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cGunluk).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cHaftalik).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cMonthly).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cGunluk;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cHaftalik;
+        private System.Windows.Forms.DataVisualization.Charting.Chart cMonthly;
     }
 }
--- WFA_YemekKaloriHesap_Proje/Profil.Designer.cs
+++ WFA_YemekKaloriHesap_Proje/Profil.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             lnkLblSifreDegistir = new LinkLabel();
             label1 = new Label();
             lblAdSoyad = new Label();
@@ -153,17 +153,19 @@
             // 
             // chartFavoriBesin
             // 
-            chartArea2.Name = "ChartArea1";
-            chartFavoriBesin.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            chartFavoriBesin.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            chartFavoriBesin.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend1.Name = "Legend1";
+            chartFavoriBesin.Legends.Add(legend1);
             chartFavoriBesin.Location = new Point(713, 91);
             chartFavoriBesin.Name = "chartFavoriBesin";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            chartFavoriBesin.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chartFavoriBesin.Series.Add(series1);
             chartFavoriBesin.Size = new Size(350, 328);
             chartFavoriBesin.TabIndex = 16;
             chartFavoriBesin.Text = "chart2";
@@ -213,6 +215,7 @@
             lLabelGrafikler.TabIndex = 22;
             lLabelGrafikler.TabStop = true;
             lLabelGrafikler.Text = "Kalori Takibim";
+            lLabelGrafikler.LinkClicked += lLabelGrafikler_LinkClicked;
             // 
             // label4
             // 
